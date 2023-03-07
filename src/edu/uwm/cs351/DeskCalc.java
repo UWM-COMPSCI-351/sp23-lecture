69b853cfe10a861c3d4f92b73a3012b83401bc8f
boyland <boyland@UWM014851.ad.uwm.edu>
2023-03-07 10:19:52 -0600

    after lecture

--- src/edu/uwm/cs351/DeskCalc.java
+++ src/edu/uwm/cs351/DeskCalc.java
@@ -3,18 +3,35 @@ package edu.uwm.cs351;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.StreamTokenizer;
+import java.util.Stack;
 
 public class DeskCalc {
 
-	final StreamTokenizer tokens;
+	private final StreamTokenizer tokens;
+	private final Stack<Double> stack;
 	
 	public DeskCalc(StreamTokenizer st) {
 		tokens = st;
+		stack = new Stack<>();
 	}
 	
 	public void run() throws IOException {
 		while (tokens.nextToken() != StreamTokenizer.TT_EOF) {
 			switch (tokens.ttype) {
+			case StreamTokenizer.TT_NUMBER:
+				stack.push(tokens.nval);
+				break;
+			case '+': {
+				double n2 = stack.pop();
+				double n1 = stack.pop();
+				stack.push(n1+n2);
+			}
+			break;	
+			case '=': {
+				double n = stack.peek();
+				System.out.println(n);
+			}
+			break;
 			default:
 				if (tokens.ttype < 0) {
 					System.out.println("Cannot handle " + tokens.sval);
