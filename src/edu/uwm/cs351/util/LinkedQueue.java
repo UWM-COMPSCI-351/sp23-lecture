936bd04f8e23ab407b93c987ec56cd67ae29e8e8
boyland <boyland@UWM014851.ad.uwm.edu>
2023-03-09 10:18:26 -0600

    after lecture

--- src/edu/uwm/cs351/util/LinkedQueue.java
+++ src/edu/uwm/cs351/util/LinkedQueue.java
@@ -1,30 +1,56 @@
 package edu.uwm.cs351.util;
 
+import java.util.NoSuchElementException;
+
 public class LinkedQueue<E> implements Queue<E>, Cloneable {
 
+	private static class Node<T> {
+		T data;
+		Node<T> next;
+		Node(T d, Node<T> n) { data = d; next = n; }
+	}
+	
+	private Node<E> head, tail;
+	
 	@Override // required
 	public void enqueue(E elem) {
-		// TODO
+		if (elem == null) throw new NullPointerException();
+		Node<E> n = new Node<>(elem, null);
+		if (tail != null) {
+			tail.next = n;
+		} else {
+			head = n;
+		}
+		tail = n;
 	}
 	
 	@Override // required
 	public E dequeue() {
-		return null; // TODO
+		if (head == null) throw new NoSuchElementException("");
+		E result = head.data;
+		head = head.next;
+		if (head == null) tail = null;
+		return result;
 	}
 	
 	@Override // required
 	public E front() {
-		return null; // TODO
+		if (head == null) throw new NoSuchElementException("");
+		return head.data;
 	}
 	
 	@Override // required
 	public int size() {
-		return -1; // TODO
+		int count = 0;
+		for (Node<E> n = head; n != null; n = n.next) {
+			++count;
+		}
+		return count;
 	}
 	
 	@Override // required
 	public boolean isEmpty() {
-		return false;
+		return head == null;
 	}
 	
 	@Override // required
